eddac26b1020974ffd31633ab75e57ee0bc54857
katanotetsuya <katanotetsuya0812@gmail.com>
2020-10-31 21:09:42 +0900

    test デプロイ

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '6d4633ef01514c0be4f85baf512f03460fdaf2e7d78161e13aa525db141785bd5ab7f0edd0adf9d77e0bed7686dbe36cccb8961b0ae9e7f8545aa6339a1d8d0f'
